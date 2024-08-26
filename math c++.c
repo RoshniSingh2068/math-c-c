d7e92bec4d6a0931c413c4bbe9461df89078a4ff
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:18:16 +0530

    Create math c++.c

---
+++ math c++.c
@@ -0,0 +1,10 @@
+// math
+// has many function that allow you to perform mathematical task
+// max
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout << max (5,10);
+    return 0;
+}
